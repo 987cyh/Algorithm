d98c1d4efd3c462f223d84a0b5cbfb3096de8ac5
987cyh <987cyh@gmail.com>
2023-06-11 19:42:44 +0900

    표본평균, 모평균

---
+++ 03 Statistical Basics/07 표본평균, 모평균.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+□ 참고 : 파이썬으로 배우는 통계학 교과서
+□ 목적 : 표본평균, 모평균 복습
+"""
+#%%
+# package
+import numpy as np
+import scipy as sp
+from scipy import stats
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+#%%
+# 표본평균
+population = stats.norm(loc=4, scale=0.8)
+
+sample_mean_array = np.zeros(10000)
+
+np.random.seed(1)
+for k in range(0, 10000):
+    sample = population.rvs(size=10)
+    sample_mean_array[k] = np.mean(sample)
+
+print(sample_mean_array)
+print(np.mean(sample_mean_array))
+print(np.std(sample_mean_array))
+sns.distplot(sample_mean_array,color='black')
+plt.show()
+#%%
+size_array = np.arange(start=10, stop=100100, step=100)
+print(size_array)
+
+sample_mean_array_size = np.zeros(len(size_array))
+print(sample_mean_array_size)
+
+np.random.seed(1)
+for k in range(0, len(size_array)):
+    sample = population.rvs(size=size_array[k])
+    sample_mean_array_size[k] = np.mean(sample)
+
+plt.plot(size_array, sample_mean_array_size,color='black')
+plt.xlabel('sample size')
+plt.ylabel('sample mean')
+plt.show()
+#%%
