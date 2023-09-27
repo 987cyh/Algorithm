83501b9c713a9a31b3a6e79bff7fa6cd175e4cdb
987cyh <987cyh@gmail.com>
2023-09-27 22:23:08 +0900

    PCA, Vector

--- 01 Kloud_study/season2/02_PCA.py
+++ 01 Kloud_study/season2/02_PCA.py
@@ -1,56 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Kloud
+
+@student: 987cyh
+"""
+
+# package
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-
-
-
-#임의의 분포를 만든다
+#%%
+#임의의 분포를 생성
 X = np.random.normal(0,1,1000)
-
 Y = X * 1 + np.random.normal(0,1,1000)
-
-Y = np.random.normal(0,1,1000)
-
-Y = X * -1 + np.random.normal(0, 1,1000)
-
-
-
 #분포 출력
 plt.scatter(X, Y)
 plt.show()
-
-
-
-
-#개별 분산 및 공분산을 계산한다.
+#개별 분산 및 공분산을 계산
 공분산 = np.cov(X, Y)
 print(공분산)
 
+X = np.random.normal(0,1,1000)
+Y = np.random.normal(0,1,1000)
+#분포 출력
+plt.scatter(X, Y)
+plt.show()
+#개별 분산 및 공분산을 계산
+공분산 = np.cov(X, Y)
+print(공분산)
 
-
+X = np.random.normal(0,1,1000)
+Y = X * -1 + np.random.normal(0, 1,1000)
+#분포 출력
+plt.scatter(X, Y)
+plt.show()
+#개별 분산 및 공분산을 계산
+공분산 = np.cov(X, Y)
+print(공분산)
+#%%
 #공분산 행렬의 의미
 균등좌표 = (np.arange(41) -20) * 0.1
 균등좌표 = np.full([41,41], 균등좌표)
 균등좌표 = np.concatenate([균등좌표[:,:, np.newaxis], 균등좌표.T[:,:, np.newaxis]], axis=2)
 
-
 #균등좌표계 출력
 plt.scatter(균등좌표[:,:,0], 균등좌표[:,:,1],s=0.5)
 plt.show()
 
-
-
 #공분산 행렬로 변환
 변환좌표 = 균등좌표 @ 공분산
-
-
 
 #변환된 좌표 출력
 plt.scatter(변환좌표[:,:,0], 변환좌표[:,:,1],s=0.5)
 plt.show()
-
-
 
 #고유벡터
 각도 = np.arange(360)
@@ -62,16 +65,12 @@ plt.show()
 plt.scatter(원래각도[:,0], 원래각도[:,1],s=0.5)
 plt.show()
 
-
-
 #공분산행렬로 변환한다.
 행렬변환 = 원래각도 @ 공분산
 
 plt.scatter(행렬변환[:,0], 행렬변환[:,1],s=0.5)
 plt.show()
-
-
-
+#%%
 def dist(v):
     return math.sqrt(v[0]**2 + v[1]**2)
 
@@ -103,7 +102,6 @@ def getDegree(v):
 기준벡터 = np.concatenate([기준벡터, 행렬변환], axis=1)
 
 변환각도 = np.apply_along_axis(getDegree, 1, 기준벡터) / 180 * np.pi
-
 변환각도 = np.cos(변환각도)
 
 각도차이 = 각도_X - 변환각도
@@ -111,28 +109,22 @@ def getDegree(v):
 
 np.where(각도차이 < 0.0001)
 
-
 plt.scatter(원래각도[:,0], 원래각도[:,1],s=0.5)
 plt.scatter(원래각도[np.where(각도차이 < 0.0001),0], 원래각도[np.where(각도차이 < 0.0001),1],s=5, marker='X', color='#FF0000')
 plt.show()
-
 
 plt.scatter(행렬변환[:,0], 행렬변환[:,1],s=0.5)
 plt.scatter(행렬변환[np.where(각도차이 < 0.0001),0], 행렬변환[np.where(각도차이 < 0.0001),1],s=5, marker='X', color='#FF0000')
 plt.show()
 
-
 plt.scatter(X, Y, alpha=0.2)
 plt.scatter(행렬변환[:,0], 행렬변환[:,1],s=0.5)
 plt.scatter(행렬변환[np.where(각도차이 < 0.00001),0], 행렬변환[np.where(각도차이 < 0.00001),1],s=20, marker='X', color='#FF0000')
 plt.show()
-
-
-
+#%%
 """===============================================================================
 주성분 분석
 ==============================================================================="""
-
 from sklearn.decomposition import PCA
 data = np.concatenate([X[:,np.newaxis],Y[:,np.newaxis]], axis=1)
 
@@ -148,4 +140,4 @@ pre2 = data @ pca.components_.T
 
 plt.scatter(predict[:,0], predict[:,1])
 plt.show()
-
+#%%
